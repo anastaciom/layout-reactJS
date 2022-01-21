9c59eed145f6a2bbce05e3ac3c87c4accfaa10cc
Anastacio Menezes <anastaciomenezes@outlook.com>
2022-01-21 14:31:14 -0300

    feat: creating the main part of the layout
    Improved some styling details in the header and created the main part of the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Banner from './components/Banner';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Banner/>
     </div>
   );
 }
---
+++ src/components/Banner.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import imgBackground from "../assets/fireball.gif";
+import {
+  PlayCircle,
+  Instagram,
+  Facebook,
+  Twitter
+} from "@mui/icons-material/";
+export default function Banner() {
+  return (
+    <main id="bannerArea">
+      <aside id="contactArea">
+      
+       
+        <Instagram style={{color: '#fff', fontSize: 25}}/>
+        <Facebook style={{color: '#fff', fontSize: 25}}/>
+        <Twitter style={{color: '#fff', fontSize: 25}}/>
+        
+      
+      </aside>
+
+      <section id="mainInfo">
+        <div id="bannerInfo">
+          <div id="description">
+            <h1>Discover the new reality</h1>
+            <p>
+              We've entered into a New Reality and Discover our approach to
+              business transformation four strategic
+              angles_____________________________
+            </p>
+          </div>
+
+          <div id="infoCard">
+            <span id="boxVideo">
+              <PlayCircle
+                style={{ fontSize: 50, color: "#fc5560", cursor: "pointer" }}
+              />
+            </span>
+            <span id="card-description">
+              <h3>Meet with the future</h3>
+              <small>Technology</small>
+            </span>
+          </div>
+        </div>
+        <div id="imgBackground">
+          <img src={imgBackground} alt="imgBackground" />
+        </div>
+      </section>
+    </main>
+  );
+}
